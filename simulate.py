9a5cd5266662449cfc9e3443c879f00e92443494
Jared Krogsrud <JKrogsrud@gmail.com>
2023-01-17 18:48:30 -0500

    Added gravity and a floor

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Location for many built-in files
+
+p.setGravity(0, 0, -9.8)  # Set gravity downward gravitational pull
+
+p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 
