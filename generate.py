8abf60692e959de4af76b2f334e0ee9f97fd3aee
Jared Krogsrud <JKrogsrud@gmail.com>
2023-01-17 19:15:24 -0500

    Monolith no longer springs from floor

--- generate.py
+++ generate.py
@@ -10,7 +10,11 @@ length = 1
 width = 2
 height = 3
 
+x = 0
+y = 0
+z = 1.5
+
 # Creates a cube and sends it to box. Units are in meters.
-pyrosim.Send_Cube(name='box', pos=[0, 0, 0.5], size=[length, width, height])
+pyrosim.Send_Cube(name='box', pos=[x, y, z], size=[length, width, height])
 
 pyrosim.End()  # Close sdf file
