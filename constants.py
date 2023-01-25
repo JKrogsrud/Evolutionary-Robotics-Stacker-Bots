e26c1d9c3e19021447eeb72e5a06f204ddddae5a
Jared Krogsrud <JKrogsrud@gmail.com>
2023-01-25 12:45:53 -0500

    Refactored comments Creating Class hierarchy for better reading

--- constants.py
+++ constants.py
@@ -1,14 +1,35 @@
 import numpy as np
 
-# ROBOT, PLANE and WORLD filenames
+"""SIMULATION CONSTANTS"""
+SLEEP_TIME = 1/240
+
+"""ROBOT, PLANE and WORLD filenames"""
 PLANE = "plane.urdf"
 BOT_1 = "body.urdf"
 WORLD = "world.sdf"
 
-# DATA COLLECTION
+"""DATA COLLECTION"""
 SIM_LEN = 5000
 
-# GRAVITY CONSTANTS
+"""GRAVITY CONSTANTS"""
 X_GRAV = 0
 Y_GRAV = 0
 Z_GRAV = -9.8
+
+"""
+BOT CONSTANTS
+"""
+# Frame repetition
+FRAMES = 1000
+
+# Back Leg values
+BACK_AMP = 4 * np.pi / 16
+BACK_FREQ = 6.1
+BACK_PHASE = -4 * np.pi / 16
+BACK_FORCE = 100
+
+# Front Leg Values
+FRONT_AMP = 3 * np.pi / 16
+FRONT_FREQ = 6.1
+FRONT_PHASE = 0
+FRONT_FORCE = 100
--- simulate.py
+++ simulate.py
@@ -6,58 +6,50 @@ import numpy as np  # For arrays to store sensor values
 import random  # For randomized Motor control
 import constants as c  # File in which we store many of the constants we are using
 
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Location for many built-in files
+pass
 
-# Set Gravity
-p.setGravity(c.X_GRAV, c.Y_GRAV, c.Z_GRAV)  # Set gravity downward gravitational pull
-
-p.loadURDF(c.PLANE)
-robotID = p.loadURDF(c.BOT_1)
-p.loadSDF(c.WORLD)
-
-pyrosim.Prepare_To_Simulate(robotID)  # Extra little work to have sensors set up for this robot
-# Would need to iterate through an array of robots if you wanted a swarm
-
-backLegSensorValues = np.zeros(c.SIM_LEN)
-frontLegSensorValues = np.zeros(c.SIM_LEN)
-
-# Some values to help with motor control
-back_amplitude = 4*np.pi/16
-back_frequency = 6.1
-back_phaseOffset = -4*np.pi/16
-
-back_x = np.linspace(start=0-back_phaseOffset, stop=2*np.pi-back_phaseOffset, num=1000)*back_frequency
-
-targetAnglesBack = np.sin(back_x)*back_amplitude
-
-front_amplitude = 3*np.pi/16
-front_frequency = 6.1
-front_phaseOffset = 0
-
-front_x = np.linspace(start=0-front_phaseOffset, stop=2*np.pi-front_phaseOffset, num=1000)*front_frequency
-
-targetAnglesFront = np.sin(front_x)*front_amplitude
-
-np.save('data/targetAnglesBack.npy', targetAnglesBack)
-np.save('data/targetAnglesFront.npy', targetAnglesFront)
-
-for i in range(c.SIM_LEN):
-    p.stepSimulation()
-    #backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotID,
-                                jointName="Torso_BackLeg",
-                                controlMode=p.POSITION_CONTROL,
-                                targetPosition=targetAnglesBack[i % 1000],
-                                maxForce=100)
-    #frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotID,
-                                jointName="Torso_FrontLeg",
-                                controlMode=p.POSITION_CONTROL,
-                                targetPosition=targetAnglesFront[i % 1000],
-                                maxForce=100)
-    time.sleep(1/240)
-
-#np.save('./data/backLegSensorValues.npy', backLegSensorValues)
-#np.save('./data/FrontLegSensorValues.npy', frontLegSensorValues)
-p.disconnect()
+# physicsClient = p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Location for many built-in files
+#
+# # Set Gravity
+# p.setGravity(c.X_GRAV, c.Y_GRAV, c.Z_GRAV)  # Set gravity downward gravitational pull
+#
+# p.loadURDF(c.PLANE)
+# robotID = p.loadURDF(c.BOT_1)
+# p.loadSDF(c.WORLD)
+#
+# pyrosim.Prepare_To_Simulate(robotID)  # Extra little work to have sensors set up for this robot
+# # Would need to iterate through an array of robots if you wanted a swarm
+#
+# backLegSensorValues = np.zeros(c.SIM_LEN)
+# frontLegSensorValues = np.zeros(c.SIM_LEN)
+#
+# # Create motor schedule for bot limbs
+# back_x = np.linspace(start=0-c.BACK_PHASE, stop=2*np.pi-c.BACK_PHASE, num=c.FRAMES)*c.BACK_FREQ
+# targetAnglesBack = np.sin(back_x)*c.BACK_AMP
+#
+# front_x = np.linspace(start=0-c.FRONT_PHASE, stop=2*np.pi-c.FRONT_PHASE, num=c.FRAMES)*c.FRONT_FREQ
+# targetAnglesFront = np.sin(front_x)*c.FRONT_AMP
+#
+# np.save('data/targetAnglesBack.npy', targetAnglesBack)
+# np.save('data/targetAnglesFront.npy', targetAnglesFront)
+#
+# for i in range(c.SIM_LEN):
+#     p.stepSimulation()
+#     #backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+#     pyrosim.Set_Motor_For_Joint(bodyIndex=robotID,
+#                                 jointName="Torso_BackLeg",
+#                                 controlMode=p.POSITION_CONTROL,
+#                                 targetPosition=targetAnglesBack[i % c.FRAMES],
+#                                 maxForce=c.BACK_FORCE)
+#     #frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+#     pyrosim.Set_Motor_For_Joint(bodyIndex=robotID,
+#                                 jointName="Torso_FrontLeg",
+#                                 controlMode=p.POSITION_CONTROL,
+#                                 targetPosition=targetAnglesFront[i % c.FRAMES],
+#                                 maxForce=c.FRONT_FORCE)
+#     time.sleep(c.SLEEP_TIME)
+#
+# #np.save('./data/backLegSensorValues.npy', backLegSensorValues)
+# #np.save('./data/FrontLegSensorValues.npy', frontLegSensorValues)
+# p.disconnect()
