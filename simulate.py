705b523115bab13b3f21de7276c6ae91e5790a07
Jared Krogsrud <JKrogsrud@gmail.com>
2023-03-03 15:37:12 -0500

    Robot partially complete

--- simulate.py
+++ simulate.py
@@ -4,10 +4,11 @@ import sys
 
 directOrGUI = sys.argv[1]
 solutionID = sys.argv[2]
+bodyType = sys.argv[3]
 
-simulation = SIMULATION(directOrGUI, solutionID)
+simulation = SIMULATION(directOrGUI, solutionID, bodyType)
 
 simulation.run()
 
-simulation.Get_Fitness()
+# simulation.Get_Fitness()
 
